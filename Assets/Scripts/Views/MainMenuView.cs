f014560aec7fa84aff6e99080617490aaeacdda0
Daniele Montesano <damonte91@hotmail.it>
2024-03-10 00:20:02 +0900

    modify icon to change main menu soundtrack

--- Assets/Scripts/Views/MainMenuView.cs
+++ Assets/Scripts/Views/MainMenuView.cs
@@ -7,18 +7,25 @@ public class MainMenuView : MonoBehaviour
     [SerializeField]
     private SerializableReactiveProperty<bool> _oldStyleSoundtrack = new SerializableReactiveProperty<bool>(false);
     [SerializeField] private Button _soundtrackSwapper;
+    [SerializeField] private Image _soundtrackButtonIcon;
     [SerializeField] private AudioSource _soundtrackPlayer;
     [SerializeField] private AudioClip _oldSoundtrack;
-    [SerializeField] private AudioClip _newSoundtrack;   
+    [SerializeField] private AudioClip _newSoundtrack;
+    [SerializeField] private Sprite _oldPickachu;
+    [SerializeField] private Sprite _newPickachu;
 
 
     private void Start()
     {
         _oldStyleSoundtrack
             .Where(isOld => isOld)
-            .Select(_ => _oldSoundtrack)
-            .Merge(_oldStyleSoundtrack.Where(isOld => !isOld).Select(_ => _newSoundtrack))
-            .Subscribe(SwapSoundtrack)
+            .Select(_ => (_oldSoundtrack,_newPickachu))
+            .Merge(_oldStyleSoundtrack.Where(isOld => !isOld).Select(_ => (_newSoundtrack,_oldPickachu)))
+            .Subscribe(data =>
+            {
+                SwapSoundtrack(data.Item1);
+                _soundtrackButtonIcon.sprite = data.Item2;
+            })
             .AddTo(this);
 
         _soundtrackSwapper
